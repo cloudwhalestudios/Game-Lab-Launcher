9221bba880079afae5a2ab8bc309df3c95e3e282
Daniel <arctic-crow@web.de>
2019-04-06 15:31:47 +0200

    WebGL Testing Part 1

---
+++ 2ButtonLauncher/Assets/Scripts/WebGL/Testing/TestWebGL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestWebGL : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void OpenGame(string location);
+    [DllImport("__Internal")]
+    private static extern void Reload();
+    [DllImport("__Internal")]
+    private static extern void Crash();
+
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+
+    private void Start()
+    {
+        Hello();
+    }
+
+    public void WebGLOpenGame(string location)
+    {
+        OpenGame(location);
+    }
+
+    public void WebGLReload()
+    {
+        Reload();
+    }
+
+    public void WebGLCrash()
+    {
+        Crash();
+    }
+}
