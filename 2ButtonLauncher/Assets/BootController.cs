a6a1bdd94e9c598413075c28766ee833edae2261
Daniel <arctic-crow@web.de>
2019-04-06 22:08:24 +0200

    Boot Screen

---
+++ 2ButtonLauncher/Assets/BootController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootController : MonoBehaviour
+{
+    public float minBootDelay = 2f;
+    public string mainSceneName;
+    public string setupSceneName;
+
+    bool userIsSetup = false;
+
+    private void Awake()
+    {
+        // TODO Add loading behaviour
+        userIsSetup = true;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartNextScene());
+    }
+
+    IEnumerator StartNextScene()
+    {
+        yield return new WaitForSecondsRealtime(minBootDelay);
+
+        if (userIsSetup)
+        {
+            SceneManager.LoadScene(mainSceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(setupSceneName);
+        }
+        yield break;
+    }
+}
