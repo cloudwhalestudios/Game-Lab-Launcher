f3fced6bca0b6e45daedc4b9dd25135cd83f79ee
Marcel <34310427+Selians@users.noreply.github.com>
2019-04-06 20:38:59 +0200

    Launcher assets + setup
    Added assets & UI setup for the launcher

---
+++ 2ButtonLauncher/Assets/Scripts/UI/ScrollImage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollImage : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    public float speed;
+    public Vector3 startOffset;
+    public float startPoint;
+    public float endPoint;
+
+    private void Start()
+    {
+        transform.position += startOffset;
+    }
+
+    void Update()
+    {
+        if (((RectTransform)transform).pivot.x <= endPoint)
+        {
+            transform.position = new Vector3(startPoint, transform.position.y, transform.position.z);
+        }
+        
+        else
+        {
+            speed = scrollSpeed * Time.deltaTime;
+
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+    }
+}
