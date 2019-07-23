7fc7d29160628c30b47d1657e61b3608b9ecac35
Marcel <34310427+Selians@users.noreply.github.com>
2019-07-23 12:55:42 +0200

    Language manager push
    No large changes, switching PC's

---
+++ 2ButtonLauncher/Assets/Scripts/LanguageManager/LanguageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour
+{
+    public string selectedLang;
+    public string backupLang = "Lan_EN";
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
